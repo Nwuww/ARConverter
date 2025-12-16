584baeabc0ea0a26e4215a96922cc9ef27358f40
Nwuww <wunanfred2025@163.com>
2025-12-16 04:22:03 +0000

    文件类型检测

--- ARConverter/Program.cs
+++ ARConverter/Program.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using ARConverter.Utils;
 
 namespace ARConverter;
 public static class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
+        string src_path = args[0] ?? InputPath(); // 拖入文件时直接解析
         
+
+    }
+
+    static string InputPath()
+    {
+        Console.WriteLine("请输入文件路径：");
+        string input = Console.ReadLine() ?? string.Empty;
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("路径不能为空，请重新输入！");
+            return InputPath();
+        }
+        return input;
     }
 }
---
+++ ARConverter/Utils/FileDetector.cs
@@ -0,0 +1,31 @@
+﻿// FileDetector.cs
+// 检测输入文件的类型
+// ArcCreate or Arcade or Unknown
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARConverter.Utils;
+
+public enum FileType
+{
+    ArcCreate,
+    Arcade,
+    Unknown
+}
+public class FileDetector
+{
+    static public FileType Detect(string? filename)
+    {
+        if (!File.Exists(filename))
+            throw new Exception("没有找到" + filename + "喵");
+
+        return Path.GetExtension(filename).ToUpper() switch
+        {
+            null => FileType.Unknown,
+            _ when filename.EndsWith(".ZIP") => FileType.Arcade,
+            _ when filename.EndsWith(".ARCPKG") => FileType.ArcCreate,
+            _ => FileType.Unknown
+        };
+    }
+}
